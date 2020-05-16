78922aec90f43efeaf5dedf916a7e62e76c9563f
Himanshu Rathore <36191619+himanshu-802@users.noreply.github.com>
2020-05-16 11:12:43 +0530

    Update test_flaskr.py

--- projects/02_trivia_api/starter/backend/test_flaskr.py
+++ projects/02_trivia_api/starter/backend/test_flaskr.py
@@ -34,7 +34,7 @@ class TriviaTestCase(unittest.TestCase):
     Write at least one test for each test for successful operation and for expected errors.
     """
 
-     def test_get_paginated_questions(self):
+    def test_get_paginated_questions(self):
         res = self.client().get('/questions')
         data = json.loads(res.data)
 
